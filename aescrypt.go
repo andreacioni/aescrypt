15b2b0983ddeb203c098db19c6af6948e41a6e85
Andrea <supercionci@hotmail.it>
2018-02-27 12:57:21 +0100

    WIP

--- aescrypt.go
+++ aescrypt.go
@@ -7,9 +7,11 @@ import (
 	"crypto/hmac"
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/binary"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"reflect"
 )
 
 type AESVersion byte
@@ -17,6 +19,7 @@ type AESVersion byte
 const (
 	AESCryptVersion1 AESVersion = 0x01
 	AESCryptVersion2 AESVersion = 0x02
+	BlockSizeBytes              = 32
 	KeySizeBytes                = 32
 	IVSizeBytes                 = 16
 )
@@ -36,22 +39,32 @@ func New(ver AESVersion, key string) *AESCrypt {
 	}
 }
 
+func NewV1(key string) *AESCrypt {
+	return New(AESCryptVersion1, key)
+}
+
+func NewV2(key string) *AESCrypt {
+	return New(AESCryptVersion2, key)
+}
+
 func (c *AESCrypt) Encrypt(fromPath, toPath string) error {
 
 	plainFile, err := os.Open(fromPath)
 
 	if err != nil {
-		return fmt.Errorf("Unable to open the file to encrypt: %v", fromPath)
+		return fmt.Errorf("unable to open the file to encrypt: %v", fromPath)
 	}
 
 	src, err := ioutil.ReadAll(plainFile)
 
 	if err != nil {
-		return fmt.Errorf("Unable to read the file to encrypt: %v", fromPath)
+		return fmt.Errorf("unable to read the file to encrypt: %v", fromPath)
 	}
 
 	iv1 := generateRandomIV()
 	iv2 := generateRandomIV()
+	aesKey1 := c.derivedKey[:]
+	aesKey2 := generateRandomAESKey()
 
 	var dst *bytes.Buffer
 
@@ -64,16 +77,97 @@ func (c *AESCrypt) Encrypt(fromPath, toPath string) error {
 		dst.WriteByte(0x00) //No extension
 	}
 
-	dst.Write(iv1)                                   //16 bytes for Initialization Vector
-	ivKeyEnc := encryptIVAndKey(iv, c.derivedKey[:]) // Encrypted IV + key
+	dst.Write(iv1)                                             //16 bytes for Initialization Vector
+	ivKeyEnc := encrypt(aesKey1, iv1, append(iv2, aesKey2...)) // Encrypted IV + key
 	dst.Write(ivKeyEnc)
-	dst.Write(evaluateHMAC(c.derivedKey[:], ivKeyEnc)) // HMAC(Encrypted IV + key)
+	dst.Write(evaluateHMAC(aesKey1, ivKeyEnc)) // HMAC(Encrypted IV + key)
 
-	fmt.Println(src)
+	lastBlockLength := byte((len(src) % BlockSizeBytes) & 0x0f)
+	cipherData := encrypt(aesKey2, iv2, src)
+	dst.Write(cipherData)
+	dst.WriteByte(lastBlockLength)
+	dst.Write(evaluateHMAC(aesKey2, cipherData))
+
+	err = ioutil.WriteFile(toPath, dst.Bytes(), 0600)
+
+	if err != nil {
+		return fmt.Errorf("failed to write to destination file: %v", err)
+	}
 	return nil
 }
 
 func (c *AESCrypt) Decrypt(fromPath, toPath string) error {
+	cipherFile, err := os.Open(fromPath)
+
+	if err != nil {
+		return fmt.Errorf("unable to open the file to decrypt: %v", fromPath)
+	}
+
+	src, err := ioutil.ReadAll(cipherFile)
+
+	if err != nil {
+		return fmt.Errorf("unable to read the file to decrypt: %v", fromPath)
+	}
+
+	if !reflect.DeepEqual(src[:3], []byte("AES")) {
+		return fmt.Errorf("invalid file supplied. Are you sure it was encrypted with AESCrypt?")
+	}
+
+	switch src[3] {
+	case byte(AESCryptVersion2):
+		ivIndex, err := skipExtension(src)
+		if err != nil {
+			return fmt.Errorf("invalid extension found: %v", err)
+		}
+		src = src[ivIndex:]
+		break
+	case byte(AESCryptVersion1):
+		src = src[5:]
+		break
+	default:
+		return fmt.Errorf("version %d not supported", src[3])
+	}
+
+	if len(src) < IVSizeBytes {
+		return fmt.Errorf("IV not found")
+	}
+
+	iv1 := src[:IVSizeBytes]
+	aesKey1 := c.derivedKey[:]
+
+	src = src[IVSizeBytes:] //Skip to encrypted IV+KEY
+
+	if len(src) < IVSizeBytes+KeySizeBytes {
+		return fmt.Errorf("encrypted IV+KEY not found")
+	}
+
+	ivKey := decrypt(aesKey1, iv1, src[:IVSizeBytes+KeySizeBytes])
+
+	src = src[IVSizeBytes+KeySizeBytes:] //Skip to HMAC
+
+	if len(src) < KeySizeBytes {
+		return fmt.Errorf("first HMAC not found")
+	}
+
+	if !hmac.Equal(evaluateHMAC(aesKey1, ivKey), src[:KeySizeBytes]) {
+		return fmt.Errorf("first HMAC doesn't match, entered password is not valid")
+	}
+
+	iv2 := ivKey[:IVSizeBytes]
+	aesKey2 := ivKey[IVSizeBytes:]
+
+	src = src[KeySizeBytes:]
+
+	var dst *bytes.Buffer
+
+	if len(src) < KeySizeBytes+1 { //HMAC + size byte
+		return fmt.Errorf("no enough bytes for encrypted message")
+	} else if len(src) > KeySizeBytes+1 { //Empty message not proceed inside this block
+		cipherData := src[:len(src)-KeySizeBytes+1]
+		dst.Write(decrypt(aesKey2, iv2, cipherData))
+
+	}
+
 	return nil
 }
 
@@ -84,28 +178,72 @@ func (c *AESCrypt) getIV() []byte {
 	return c.iv
 }
 
-func encryptIVAndKey(iv1, iv2, key1, key2 []byte) []byte {
+//skipExtension used to skip the extension part (if present).
+//It returns the index of the first byte that contain IV
+func skipExtension(src []byte) (int, error) {
+	index := 7
+
+	src = src[5:] //Skip reserved byte
+
+	for {
+		if len(src) < 2 {
+			return 0, fmt.Errorf("extension length not available")
+		}
+
+		extLen := int(binary.BigEndian.Uint16(src[:2]))
+
+		if extLen == 0 {
+			return index, nil
+		}
+
+		src = src[2:] //Skip extension length
+
+		if len(src) < int(extLen) {
+			return 0, fmt.Errorf("size not match current extension length")
+		}
+
+		index += extLen
+		src = src[index:]
+	}
+}
+
+func decrypt(key, iv, src []byte) []byte {
 	block, err := aes.NewCipher(key)
 
 	if err != nil {
 		panic(err)
 	}
 
-	cbc := cipher.NewCBCEncrypter(block, iv1)
+	cbc := cipher.NewCBCDecrypter(block, iv)
 
-	src := append(iv2, generateAESKey()...)
-	dst := make([]byte, KeySizeBytes+IVSizeBytes)
+	dst := make([]byte, len(src))
 
 	cbc.CryptBlocks(dst, src)
 
 	return dst
 }
 
-func evaluateHMAC(key []byte, data []byte) []byte {
+func encrypt(key, iv, src []byte) []byte {
+	block, err := aes.NewCipher(key)
+
+	if err != nil {
+		panic(err)
+	}
+
+	cbc := cipher.NewCBCEncrypter(block, iv)
+
+	dst := make([]byte, len(src))
+
+	cbc.CryptBlocks(dst, src)
+
+	return dst
+}
+
+func evaluateHMAC(key, data []byte) []byte {
 	return hmac.New(sha256.New, key).Sum(data)
 }
 
-func generateAESKey() []byte {
+func generateRandomAESKey() []byte {
 	return generateRandomBytesSlice(KeySizeBytes)
 }
 
--- aescrypt_test.go
+++ aescrypt_test.go
@@ -1,9 +1,9 @@
 package aescrypt
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
 	"crypto/sha256"
+	"encoding/binary"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,8 +11,15 @@ import (
 
 func TestNew(t *testing.T) {
 	password := "thisisthepassword"
-	aes := New(password)
+	aes := NewV2(password)
 
+	require.Equal(t, AESCryptVersion2, aes.version)
+	require.Equal(t, password, aes.password)
+	require.Equal(t, sha256.Sum256([]byte(password)), aes.derivedKey)
+
+	aes = NewV1(password)
+
+	require.Equal(t, AESCryptVersion1, aes.version)
 	require.Equal(t, password, aes.password)
 	require.Equal(t, sha256.Sum256([]byte(password)), aes.derivedKey)
 }
@@ -21,22 +28,33 @@ func TestThreeBytes(t *testing.T) {
 	require.Equal(t, []byte{0x41, 0x45, 0x53}, []byte("AES"))
 }
 
+func TestRandomBytes(t *testing.T) {
+	b := generateRandomBytesSlice(10)
+
+	require.Equal(t, 10, len(b))
+
+}
+
 func TestIVKeyEncryption(t *testing.T) {
-	iv := []byte("ffffffffffffffff")                  //66 hex
-	key := []byte("ffffffffffffffffaaaaaaaaaaaaaaaa") //66 hex + 61 hex
-
-	require.Equal(t, 16, len(iv))
-	require.Equal(t, 32, len(key))
-
-	aesCipher, err := aes.NewCipher(key)
-
-	require.NoError(t, err)
-
-	cbcEncrypter := cipher.NewCBCEncrypter(aesCipher, iv)
-
-	encryptIVAndKey(cbcEncrypter, iv, key)
+	encrypt(generateRandomAESKey(), generateRandomIV(), append(generateRandomIV(), generateRandomAESKey()...))
 }
 
 func TestEncrypt(t *testing.T) {
 
+}
+
+func TestDeepEqual(t *testing.T) {
+	src := []byte("AESADC")
+
+	require.False(t, reflect.DeepEqual(src, []byte("AES")))
+	require.True(t, reflect.DeepEqual(src[:3], []byte("AES")))
+
+	src = []byte("ADSADC")
+
+	require.False(t, reflect.DeepEqual(src, []byte("AES")))
+	require.False(t, reflect.DeepEqual(src[:3], []byte("AES")))
+}
+
+func TestExtensionBytesLengthConvert(t *testing.T) {
+	require.EqualValues(t, 24, binary.BigEndian.Uint16([]byte{0x00, 0x18}))
 }
